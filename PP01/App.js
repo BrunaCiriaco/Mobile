a3f13f995b802bf8fcbfc3c7a9dc90a63b5076ab
BCBenedito <bruna.ciriaco2000@gmail.com>
2023-03-26 15:15:09 -0300

    Projeto Pratico 01

---
+++ PP01/App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Logo from './src/container/logo'
+import List from './src/container/list'
+
+export default () => (
+    <View >
+      <Logo />
+      <List />
+    </View>
+  );
+
+
+
+
